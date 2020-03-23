a83485a86ccbb645dad237dd5bedf7a28810a43a
Arthur Passos <arthur.opassos@gmail.com>
2020-03-23 17:31:41 -0300

    Melhoria do HelloWorld

---
+++ aula_01/HelloWorld.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const app = (request, response) => {
+    console.log(request.method, request.url);
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(`Hello ${request.url.replace("/", "")}\n`);
+    response.write('Hello ' + request.url.replace("/", ""));
+    response.end();
+    
+};
+
+const server = http.createServer(app);
+
+server.listen(8081, () =>  console.log('Rodando!'));
--- aula_01/helloword.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((request, response) => {
-response.writeHead(200,{ 'Content-Type': 'text/plain '});
-response.write('Hello, World');
-response.end();
-});
-server.listen(8081);
-
